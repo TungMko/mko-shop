23dedd5ffcc249375fcb9058a1c93c57ed04c820
TungMko <fxtung99@gmail.com>
2023-04-18 23:54:05 +0900

    tungmko

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,29 +14,20 @@ if (close) {
     })
 }
 
+// logo
+const logo = document.getElementById('logo');
+
+if (logo) {
+  logo.onclick = function() {
+  window.location.href = "index.html";
+  };
+  };
+
      // ẩn hiện menu
     const items = document.getElementById('header-nav_unit');
     const sub = document.getElementById('navbar_list_sub');
     const navbarSub = document.getElementById('navbar_sub');
-    //  items.addEventListener('mouseover', function() {
-    //      if (sub.style.display === 'none'){
-    //          sub.style.display = 'flex';
-    //          items.style.background = '#f2f2f2'
-    //          items.style.color = 'red'
-    //      } else {
-    //          sub.style.display = 'none';
-    //          items.style.background = 'none'
-    //          items.style.color = '#333'
-    //      }
-    //  });
 
-    //  const logo = document.getElementById('logo');
-
-    //  if (logo) {
-    //     logo.onclick = function() {
-    //         window.location.href = "index.html";
-    //       };
-    //     };
     items.addEventListener("mouseenter", function(event) {
         sub.style.display = "flex";
       });
@@ -44,9 +35,9 @@ if (close) {
     items.addEventListener("mouseleave", function(event) {
         sub.style.display = "none";
       });
-      sub.addEventListener("mouseenter", function(event) {
+    sub.addEventListener("mouseenter", function(event) {
         sub.style.display = "flex";
       });
-      sub.addEventListener("mouseleave", function(event) {
+    sub.addEventListener("mouseleave", function(event) {
         sub.style.display = "none";
       });
